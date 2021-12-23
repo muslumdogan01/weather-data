b3636242ac87d71092632bb321fe4688383b930f
muslumdogan01 <muslumdogan01@gmail.com>
2021-12-23 17:06:37 +0300

    weather clone fix

---
+++ weather-app/src/components/Days.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card m-3" style="width: 18rem;">
+    <div class="card-body">
+      <div class="d-flex justify-content-between">
+        <div>
+          <h5 class="card-title">{{ daysData.title }}, {{ daysData.date }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{ daysData.weather }}</h6>
+        </div>
+        <div>
+          <component :is="icon"></component>
+        </div>
+      </div>
+      <div class="card-text d-flex justify-content-between">
+        <span>{{daysData.desc}}</span>
+          <span>{{daysData.deg}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const weather_map = {
+  Clear: Clear,
+  Snow: Snow,
+  Rain: Rain,
+  Clouds: Rain,
+};
+
+import Rain from "@/components/Icons/Rain.vue";
+import Snow from "@/components/Icons/Snow.vue";
+import Sun from "@/components/Icons/Sun.vue";
+import Clear from "@/components/Icons/Clear.vue";
+
+export default {
+  props: {
+    daysData: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    Rain,
+    Snow,
+    Sun,
+    Clear,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    icon() {
+      return weather_map[this.daysData?.weather];
+    },
+  },
+};
+</script>
+<style scoped>
+.card{
+  transition: 0.5s ease-in-out !important;
+}
+.card:hover{
+  border: 1px solid grey !important;
+  transform: translateY(-5px);
+  cursor: pointer;
+}
+</style>
--- weather-app/src/views/Home.vue
+++ weather-app/src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div id="wrapper" class="col d-flex w-100 justify-content-center">
+      <div class="row w-25">
+        <Today :dailyData="dailyData" :childDailyIcon="dailyIcon" />
+      </div>
+
+      <div class="row w-75 d-flex justify-content-center">
+        <Days v-for="item in daysData" :key="item.id" :daysData="item" />
+      </div>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Today from "@/components/Today.vue";
+import Days from "@/components/Days.vue";
+import db from "@/db.json";
+const axios = require("axios");
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Today,
+    Days,
+  },
+  data() {
+    return {
+      dailyData: [],
+      dailyIcon: null,
+      daysData: db.days,
+    };
+  },
+  mounted() {
+    this.getDataList();
+  },
+
+  methods: {
+    getDataList() {
+      const url = "https://api.openweathermap.org/data/2.5/onecall?";
+      const key = "&appid=acfb937c3577744b220977179eab6dd1";
+      const params = "lat=38.42&units=metric&lon=27.14&exclude=hourly&lang=tr";
+      axios
+        .get(`${url}${params}${key}`)
+        .then((response) => {
+          // handle success
+          this.dailyData = response.data.daily;
+          this.dailyIcon = response.data.daily[0];
+          console.log("t", this.dailyData);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
+    },
+  },
+};
 </script>
+
+<style scoped>
+@media (max-width: 576px) { 
+  #wrapper{
+    display: flex !important;
+    flex-direction: column !important;
+    justify-content: center !important;
+    align-items: center !important;
+  }
+  #wrapper .row{
+    width: 75% !important;
+  }
+ }
+</style>
